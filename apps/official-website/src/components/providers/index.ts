d846811b7164f2c27430e052d20cb8be62fae461
Moritz Becker <becker@ubilabs.net>
2024-09-11 02:56:40 +0200

    feat(apps/official-website): add editor context provider

--- apps/official-website/src/components/providers/index.ts
+++ apps/official-website/src/components/providers/index.ts
@@ -1,2 +1,3 @@
+export { default as EditorProvider, useEditorContext } from './editor-provider';
 export { default as ThemeProvider, useTheme } from './theme-provider';
 export { default as RouterProvider } from './router-provider';
