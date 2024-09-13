00519557356c790f430185cbe2560d507bb81331
Moritz Becker <becker@ubilabs.net>
2024-09-13 22:37:54 +0200

    fix(packages/viewer): memoize certain scene options

--- packages/viewer/src/components/scene/scene-grid.tsx
+++ packages/viewer/src/components/scene/scene-grid.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { Grid, GridProps as ThreeGridProps } from '@react-three/drei';
 
 export interface GridProps extends ThreeGridProps {
@@ -5,19 +6,22 @@ export interface GridProps extends ThreeGridProps {
 }
 
 const SceneGrid = (props: GridProps) => {
-  const { showGrid, ...gridOptions } = {
-    showGrid: false,
-    cellSize: 0.5,
-    sectionSize: 5,
-    sectionColor: 'rgb(134, 73, 33)',
-    cellColor: 'rgb(100, 100, 100)',
-    fadeDistance: 25,
-    fadeStrength: 1,
-    args: [10, 10],
-    followCamera: false,
-    infiniteGrid: true,
-    ...props,
-  };
+  const { showGrid, ...gridOptions } = useMemo(
+    () => ({
+      showGrid: false,
+      cellSize: 0.5,
+      sectionSize: 5,
+      sectionColor: 'rgb(134, 73, 33)',
+      cellColor: 'rgb(100, 100, 100)',
+      fadeDistance: 25,
+      fadeStrength: 1,
+      args: [10, 10],
+      followCamera: false,
+      infiniteGrid: true,
+      ...props,
+    }),
+    [props],
+  );
 
   return showGrid && <Grid {...(gridOptions as GridProps)} />;
 };
