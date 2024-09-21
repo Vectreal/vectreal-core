6568333c8bb180962e31208e82eaf308078ae51b
Moritz Becker <becker@ubilabs.net>
2024-09-21 18:05:02 +0200

    docs(packages/hooks): add docstrings to new functoins

--- packages/hooks/src/use-export-model/utils/file-helpers.ts
+++ packages/hooks/src/use-export-model/utils/file-helpers.ts
@@ -1,4 +1,13 @@
-// Utility function to get file extension from URI or MIME type
+/**
+ * Determines the extension of a file based on its URI and MIME type.
+ *
+ * If the MIME type is not provided, it is inferred from the URI.
+ *
+ * Returns a default `png` extension if the extension is unknown.
+ *
+ * @param {string} uri - The URI of the image
+ * @param {string} mimeType - The MIME type of the image
+ */
 export function getFileExtension(uri: string, mimeType?: string): string {
   const uriExtension = uri.split('.').pop()?.split('?')[0];
   if (uriExtension && uriExtension !== uri) return uriExtension;
@@ -10,8 +19,11 @@ export function getFileExtension(uri: string, mimeType?: string): string {
   return 'png';
 }
 
-export default getFileExtension;
-
+/**
+ * Extracts the base name of a file from its URI.
+ *
+ * @param {string} filename - The name of the file
+ */
 export function getFileBasename(filename: string) {
   return filename?.split('.').shift() || 'scene';
 }
