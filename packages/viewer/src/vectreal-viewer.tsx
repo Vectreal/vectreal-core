bf37cf28d2730950170c77c56b3135174a8cebe1
Moritz Becker <becker@ubilabs.net>
2024-10-07 02:26:27 +0200

    docs(packages/viewer): add docstring to `VectrealViewer` component

--- packages/viewer/src/vectreal-viewer.tsx
+++ packages/viewer/src/vectreal-viewer.tsx
@@ -41,6 +41,47 @@ interface VectrealViewerProps {
   loader?: JSX.Element;
 }
 
+/**
+ * A React component for rendering 3D models.
+ *
+ * This component is designed to be easily extensible and customizable. It uses the
+ * `useModelContext` hook to access the model context and loading state. It also uses the
+ * `@react-three/drei` library to render the 3D scene.
+ *
+ * If a model is not provided, the component will render a spinner until the model is loaded.
+ *
+ * The component also accepts the following props:
+ *
+ * - `model`: A 3D model to render if not provided as child or via the `ModelContext`.
+ * - `className`: An optional className to apply to the outermost container element.
+ * - `cameraOptions`: An optional object containing options for the camera.
+ * - `controlsOptions`: An optional object containing options for the OrbitControls.
+ * - `envOptions`: An optional object containing options for the environment.
+ * - `gridOptions`: An optional object containing options for the grid.
+ * - `loader`: An optional JSX element to render while the model is loading.
+ *
+ * The component will render a `Canvas` component with the provided props. It will also render a
+ * `SceneCamera`, `SceneModel`, `SceneEnvironment`, and `SceneGrid` component inside the canvas.
+ *
+ * If the `controlsOptions` prop is not provided, the component will not render any controls.
+ *
+ * The component will render any provided children inside the canvas.
+ *
+ * See [The official website]({@link https://core.vectreal.com}) or the [vctrl/viewer README]({@link https://github.com/vectreal/vectreal-core/blob/main/packages/viewer/README.md}) for more information.
+ *
+ * @example
+ * import { VectrealViewer } from '@vctrl/viewer';
+ *
+ * const MyComponent = () => {
+ *   return (
+ *     <VectrealViewer
+ *       model={model}
+ *       controlsOptions={{ maxPolarAngle: Math.PI / 2 }}
+ *       envOptions={{ stage: { preset: 'sunset' } }}
+ *     />
+ *   );
+ * };
+ */
 const VectrealViewer = ({ model, ...props }: VectrealViewerProps) => {
   const { isFileLoading } = useModelContext();
 
