5ecc5d0dd86afc24ecbc476d5a9599bee9223239
Moritz Becker <becker@ubilabs.net>
2024-10-19 16:47:42 +0200

    fix(packages/viewer): default env preset + optional background color

--- packages/viewer/src/components/scene/scene-environment.tsx
+++ packages/viewer/src/components/scene/scene-environment.tsx
@@ -4,11 +4,11 @@ import { Environment, EnvironmentProps, Stage } from '@react-three/drei';
 export interface EnvProps {
   env?: EnvironmentProps;
   stage?: ComponentProps<typeof Stage>;
-  backgroundColor: CSSProperties['backgroundColor'];
+  backgroundColor?: CSSProperties['backgroundColor'];
 }
 
 const SceneEnvironment = (props: EnvProps['env']) => {
-  return <Environment {...props} />;
+  return <Environment preset="apartment" {...props} />;
 };
 
 export default SceneEnvironment;
