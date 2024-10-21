c5a7ec18659f15cbcc6939df35657b5126bf8a94
Moritz Becker <becker@ubilabs.net>
2024-10-21 19:12:39 +0200

    fix(apps/official-website): remove useInitGA hook from app component

--- apps/official-website/src/app.tsx
+++ apps/official-website/src/app.tsx
@@ -1,11 +1,8 @@
 import { RouterProvider, ThemeProvider } from './components/providers';
-import { useInitGA } from './lib/hooks';
 
 import './globals.css';
 
 const App = () => {
-  useInitGA();
-
   return (
     <ThemeProvider defaultTheme="dark">
       <RouterProvider />
