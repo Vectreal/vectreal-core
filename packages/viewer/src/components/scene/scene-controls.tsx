d034ec09a3a5778e5821201edcab9feee14872e2
Moritz Becker <becker@ubilabs.net>
2024-10-24 19:09:54 +0200

    refactor(packages/viewer): optimize SceneControls component
    - Remove unused dependencies and code
    - Simplify the logic for enabling controls
    - Set default values for controlsTimeout and maxPolarAngle

--- packages/viewer/src/components/scene/scene-controls.tsx
+++ packages/viewer/src/components/scene/scene-controls.tsx
@@ -1,39 +1,35 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { OrbitControls, OrbitControlsProps } from '@react-three/drei';
 
 export interface ControlsProps extends OrbitControlsProps {
   controlsTimeout?: number;
 }
 
-const SceneControls = ({ enabled, ...rest }: ControlsProps) => {
+const SceneControls = ({
+  enabled,
+  controlsTimeout = 1500,
+  ...rest
+}: ControlsProps) => {
   const [isControlsEnabled, setIsControlsEnabled] = useState(false);
 
-  const controlsOptions = useMemo(
-    () => ({
-      enabled,
-      maxPolarAngle: Math.PI / 2,
-      autoRotate: true,
-      makeDefault: true,
-      ...rest,
-    }),
-    [enabled, rest],
-  );
-
-  // wait for the stage component to center the model
-  // after 1 second enable the controls
   useEffect(() => {
-    if (isControlsEnabled) return;
-
     const timeoutId = setTimeout(
       () => setIsControlsEnabled(true),
-      controlsOptions?.controlsTimeout || 1500,
+      controlsTimeout,
     );
 
     return () => clearTimeout(timeoutId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [controlsTimeout]);
 
-  return <OrbitControls {...controlsOptions} />;
+  return (
+    <OrbitControls
+      enabled={isControlsEnabled && enabled}
+      maxPolarAngle={Math.PI / 2}
+      autoRotate
+      makeDefault
+      {...rest}
+    />
+  );
 };
 
 export default SceneControls;
