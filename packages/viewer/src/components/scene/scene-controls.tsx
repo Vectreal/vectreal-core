361f6a141499590dfe7b105b7a775bc64411dec8
Moritz Becker <becker@ubilabs.net>
2024-10-29 00:35:04 +0100

    fix(packages/viewer): disabled/stuttering SceneControls component

--- packages/viewer/src/components/scene/scene-controls.tsx
+++ packages/viewer/src/components/scene/scene-controls.tsx
@@ -5,30 +5,39 @@ export interface ControlsProps extends OrbitControlsProps {
   controlsTimeout?: number;
 }
 
-const SceneControls = ({
-  enabled,
-  controlsTimeout = 1500,
-  ...rest
-}: ControlsProps) => {
+/**
+ * SceneControls component that enables orbit controls after a specified timeout.
+ *
+ * @param {ControlsProps} props - The properties for the SceneControls component.
+ * @param {number} [props.controlsTimeout=1500] - The timeout duration in milliseconds before enabling the controls.
+ * @param {Object} rest - Additional properties passed to the OrbitControls component.
+ *
+ * @returns {JSX.Element} The OrbitControls component if controls are enabled.
+ *
+ * @example
+ * <SceneControls controlsTimeout={2000} />
+ */
+const SceneControls = ({ controlsTimeout = 1500, ...rest }: ControlsProps) => {
   const [isControlsEnabled, setIsControlsEnabled] = useState(false);
 
   useEffect(() => {
-    const timeoutId = setTimeout(
-      () => setIsControlsEnabled(true),
-      controlsTimeout,
-    );
+    const timeoutId = setTimeout(() => {
+      console.log('SceneControls: useEffect');
+      setIsControlsEnabled(true);
+    }, controlsTimeout);
 
     return () => clearTimeout(timeoutId);
   }, [controlsTimeout]);
 
   return (
-    <OrbitControls
-      enabled={isControlsEnabled && enabled}
-      maxPolarAngle={Math.PI / 2}
-      autoRotate
-      makeDefault
-      {...rest}
-    />
+    isControlsEnabled && (
+      <OrbitControls
+        maxPolarAngle={Math.PI / 2}
+        autoRotate
+        makeDefault
+        {...rest}
+      />
+    )
   );
 };
 
