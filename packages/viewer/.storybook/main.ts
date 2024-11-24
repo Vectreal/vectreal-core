72e376e6073ef7e5416eb50950fbb7065ccc96e1
Moritz Becker <becker@ubilabs.net>
2024-11-24 15:15:23 +0100

    chore(packages/viewer): update Storybook configuration to use absolute paths related to storybook dep update

--- packages/viewer/.storybook/main.ts
+++ packages/viewer/.storybook/main.ts
@@ -1,14 +1,15 @@
+import { dirname, join } from 'path';
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
   addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-addon-deep-controls',
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('storybook-addon-deep-controls'),
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {
       builder: {
         viteConfigPath: 'vite.config.ts',
@@ -22,3 +23,7 @@ export default config;
 // To customize your Vite configuration you can use the viteFinal field.
 // Check https://storybook.js.org/docs/react/builders/vite#configuration
 // and https://nx.dev/recipes/storybook/custom-builder-configs
+
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
